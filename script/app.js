a543b1f2445fd41b25855eda2c27f4cc30b3f062
Boglarka Bodnar <bodnarbog94@gmail.com>
2022-10-15 13:14:25 +0200

    api update

--- script/app.js
+++ script/app.js
@@ -1,7 +1,7 @@
 let menu
 
 async function apiMenu() {
-    let response = await fetch('https://bodnarb.eu-4.evennode.com/admin')
+    let response = await fetch('https://bodnarb.eu-4.evennode.com/')
     menu = await response.json()
     etlapUpdate()
 }
